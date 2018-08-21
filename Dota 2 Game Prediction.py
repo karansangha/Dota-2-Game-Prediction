f2b8c1dfa39455523aa1254e73bc2b996644f8db
Karan Sangha <iamkaransangha@gmail.com>
2018-08-21 02:26:25 -0700

    refactor code

--- Dota 2 Game Prediction.py
+++ Dota 2 Game Prediction.py
@@ -16,46 +16,39 @@ i = 0
 count = 0
 
 # Each champion is assigned a unique id
-while i < len(theList):
-    if '1' in theList[i]:  # If team 1 wins
-        champions = theList[i].split(',')
+for item in theList:
+    if '1' in item:
+        champions = item.split(',')
         champions.pop()
-        j = 0
-        while j < 5:
+        for j in range(5):
             if champions[j] not in db:
                 db[champions[j]] = count
                 count += 1
-            j += 1
 
-    else:  # If team 2 wins
-        champions = theList[i].split(',')
+    else:
+        champions = item.split(',')
         champions.pop()
         j = 5
-        while j < 10:
+        for j in range(5, 10):
             if champions[j] not in db:
                 db[champions[j]] = count
                 count += 1
-            j += 1
-    i += 1
 
-i = 0
 dataChampions = []
 dataWinners = []
 
-while i < len(theList):
+for item in theList:
     data = []
-    champions = theList[i].split(',')
+    champions = item.split(',')
     winner = champions.pop()
     dataWinners.append(winner)
     j = 0
-    while j < len(champions):
+    for j in range(len(champions)):
         data.append(db[champions[j]])
         j += 1
     dataChampions.append(data)
-    i += 1
 
 # Import the classifier
-
 my_classifier = tree.DecisionTreeClassifier()
 
 # Train the classifier
@@ -63,25 +56,20 @@ my_classifier.fit(dataChampions, dataWinners)
 
 # Test the algorithm
 inp = int(input())
-k = 0
 inputList = []
-while k < inp:
+
+for k in range(inp):
     line = input()
     inputList.append(line)
-    k += 1
 
-j = 0
 testChampions = []
 
-while j < len(inputList):
-    stringChampions = inputList[j].split(',')
+for item in inputList:
+    stringChampions = item.split(',')
     intChampions = []
-    z = 0
-    while z < 10:
+    for z in range(10):
         intChampions.append(db[stringChampions[z]])
-        z += 1
     testChampions.append(intChampions)
-    j += 1
 
 predictions = my_classifier.predict(testChampions)
 
